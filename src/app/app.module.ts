c5599e30b65ac954ec1ecc049723716c65691423
ArclightMike <arclightmike@outlook.com>
2021-01-12 14:10:41 -0600

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, HeroesComponent, HeroDetailComponent, MessagesComponent ],
