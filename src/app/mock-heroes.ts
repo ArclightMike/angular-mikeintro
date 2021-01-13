df299c6963adb2c08b1a89e68b56be57b01d4813
ArclightMike <arclightmike@outlook.com>
2021-01-13 11:57:42 -0600

    1 file modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Captain Kirk' },
+  { id: 12, name: 'Captain Picard' },
+  { id: 13, name: 'Captain Sisko' },
+  { id: 14, name: 'Captain Janeway' },
+  { id: 15, name: 'Dr. Crusher' },
+  { id: 16, name: 'Commander Riker' },
+  { id: 17, name: 'The Doctor' },
+  { id: 18, name: 'Worf' },
+  { id: 19, name: 'Data' },
+  { id: 20, name: 'Spock' }
 ];
